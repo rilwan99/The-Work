d846cba6be0be69c3f050acd4d9c98784f62c593
rilwan99 <77623631+rilwan99@users.noreply.github.com>
2021-10-26 15:45:30 +0800

    Update subpage number

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -12,13 +12,13 @@ function Cards() {
             <CardItem
               src="https://6.vikiplatform.com/image/220415a8e07d4eed9e82373c0962ae6d.jpg?x=b&a=0x0"
               text="Inequality Today"
-              label="Adventure"
+              label="1"
               path="/Page1"
             />
             <CardItem
               src="https://i.redd.it/f462f9p6yq551.jpg"
               text="Is inequality good or bad?"
-              label="Luxury"
+              label="2"
               path="/Page2"
             />
           </ul>
@@ -26,19 +26,19 @@ function Cards() {
             <CardItem
               src="images/img-4.png"
               text="Are all players equal in squid games?"
-              label="Mystery"
+              label="3"
               path="/Page3"
             />
             <CardItem
               src="https://cdn.i-scmp.com/sites/default/files/d8/images/methode/2020/01/09/0be537fc-31e0-11ea-9400-58350050ee52_image_hires_131701.JPEG"
               text="Squid game vs reality"
-              label="Adventure"
+              label="4"
               path="/Page4"
             />
             <CardItem
               src="https://i.guim.co.uk/img/media/5f33b2fb36ae9afba06593ca64898662b483e2a3/0_210_5793_3476/master/5793.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=89e48286a6b4b4694e0602272458f751"
               text="Final insights and conclusion"
-              label="Adrenaline"
+              label="5"
               path="/Page5"
             />
           </ul>
