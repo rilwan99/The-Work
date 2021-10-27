33df93a9574d56c783273b9ecd92aef3168a1aa3
rilwan99 <77623631+rilwan99@users.noreply.github.com>
2021-10-27 23:38:47 +0800

    mute

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className="hero-container">
       {" "}
-      <video src="/videos/video-1.mov" autoPlay loop />
+      <video src="/videos/video-1.mov" autoPlay loop muted />
       <h1>SQUID GAME</h1>
       <p>And INEQUALITY</p>
     </div>
